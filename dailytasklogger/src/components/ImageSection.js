596cc62d5897b368a49799632dc716c76db3bb28
Magician <pranshuchawla19@gmail.com>
2025-02-16 19:45:22 -0500

    video added on landing page

--- dailytasklogger/src/components/ImageSection.js
+++ dailytasklogger/src/components/ImageSection.js
@@ -3,14 +3,15 @@ import styles from "../styles/ImageSection.module.css";
 
 export default function ImageSection() {
   return (
-    <div className={styles.image}>
-      <Image
-        src="/images/dailyLoggerScreenShot.png"
-        alt="Screenshot of the Daily Task Logger"
-        width={800}
-        height={600}
-        priority
-      />
+    <div className={styles.videoContainer}>
+      <iframe
+        width="800"
+        height="600"
+        src="https://www.youtube.com/embed/DlY3mBIIm4E"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
