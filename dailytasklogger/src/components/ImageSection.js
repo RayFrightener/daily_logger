f054b8cdba7da91a60c695d5f4ac282fbb7cdffb
Magician <pranshuchawla19@gmail.com>
2025-02-21 17:25:49 -0500

    accessuble on mobile devices

--- dailytasklogger/src/components/ImageSection.js
+++ dailytasklogger/src/components/ImageSection.js
@@ -5,6 +5,7 @@ export default function ImageSection() {
   return (
     <div className={styles.videoContainer}>
       <iframe
+        className={styles.videoContainer}
         width="800"
         height="600"
         src="https://www.youtube.com/embed/DlY3mBIIm4E"
